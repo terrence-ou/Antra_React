aeb51444ce511453fb49261b558aba670a2c8da3
terrence-ou <outerrencedl@gmail.com>
2024-03-10 17:25:26 -0400

    page structure finished

--- HW3_Dynamic_Content/scripts/index.js
+++ HW3_Dynamic_Content/scripts/index.js
@@ -1,7 +1,14 @@
+/* ========== Global variables ========== */
 const ITEMS_PER_PAGE = 10;
+
 let currIdx = 0;
 let numPages = 1;
-const htmlBody = document.querySelector("body");
+let fullData = null;
+
+const htmlMain = document.querySelector("main");
+const pageNavDiv = document.querySelector(".pagenav");
+
+/* ========== DOM Manipulation ========== */
 
 // First fetch data from the URL
 fetch("https://jsonplaceholder.typicode.com/posts")
@@ -15,12 +22,15 @@ fetch("https://jsonplaceholder.typicode.com/posts")
   .catch((error) => console.log(error))
   .then((data) => {
     numPages = Math.ceil(data.length / ITEMS_PER_PAGE);
+    fullData = data;
 
-    // DEV Part
-    displayDataSlice(htmlBody, 0, data);
+    // Handling contents
+    displayDataSlice(htmlMain, 0, data);
     createPaginationBar(numPages);
-    // END
+    setButtonState();
   });
+
+/* ========== Helper Functions ========== */
 
 // Get the data slice based on current page number
 function getDataSlice(pageNum, data) {
@@ -33,24 +43,29 @@ function getDataSlice(pageNum, data) {
 // Convert a single data element to an HTML element
 function createDataElement(item) {
   const elem = document.createElement("div");
-  const heading = document.createElement("h1");
-  const body = document.createElement("p");
-  heading.textContent = item.title;
-  body.textContent = item.body;
-  elem.append(heading, body);
+  elem.className = "card";
+  const title = document.createElement("h2");
+  const info = document.createElement("p");
+  const index = document.createElement("i");
+  title.textContent = item.title;
+  title.className = "card__title";
+  info.textContent = item.body;
+  info.className = "card__info";
+  index.textContent = `Item ID: ${item.id}`;
+  index.className = "card__id";
+  elem.append(title, index, info);
   return elem;
 }
 
 // Generate HTML data layout based on the current page number
 function displayDataSlice(parentElement, pageNum, data) {
+  htmlMain.innerHTML = "";
   const dataSlice = getDataSlice(pageNum, data);
   const dataElements = dataSlice.map((item) => createDataElement(item));
   parentElement.append(...dataElements);
 }
 
 function createPaginationBar(numPages) {
-  const pageBar = document.createElement("div");
-
   // Create page navigations buttons
   const prevButton = document.createElement("button");
   const nextButton = document.createElement("button");
@@ -64,11 +79,13 @@ function createPaginationBar(numPages) {
 
   // Create event listeners for each button
   const pageButtons = [prevButton, ...indexButtons, nextButton];
-  pageButtons.forEach((button) => addButtonClickEvent(button));
+  pageButtons.forEach((button) => {
+    button.className = "pagenav__btn";
+    addButtonClickEvent(button);
+  });
 
   // Append pagination bar to the HTML
-  pageBar.append(...pageButtons);
-  htmlBody.appendChild(pageBar);
+  pageNavDiv.append(...pageButtons);
 }
 
 function addButtonClickEvent(button) {
@@ -77,10 +94,31 @@ function addButtonClickEvent(button) {
     if (buttonText === "Prev") {
       currIdx = Math.max(0, currIdx - 1);
     } else if (buttonText === "Next") {
-      currIdx = Math.min(numPages, currIdx + 1);
+      currIdx = Math.min(numPages - 1, currIdx + 1);
     } else {
-      currIdx = Number(buttonText);
+      currIdx = Number(buttonText) - 1;
     }
-    console.log(currIdx);
+    displayDataSlice(htmlMain, currIdx, fullData);
+    setButtonState();
+  });
+}
+
+// Set button's display color and clickability based on the current index
+function setButtonState() {
+  const buttons = document.querySelectorAll(".pagenav__btn");
+  buttons.forEach((button) => {
+    button.classList.remove("pagenav__btn-current");
+    button.disabled = false;
+    if (Number(button.textContent) - 1 === currIdx) {
+      button.classList.add("pagenav__btn-current");
+      button.disabled = true;
+    }
+
+    if (currIdx === 0 && button.textContent === "Prev") {
+      button.disabled = true;
+    }
+    if (currIdx === numPages - 1 && button.textContent === "Next") {
+      button.disabled = true;
+    }
   });
 }
